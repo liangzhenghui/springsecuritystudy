5700613b33da5d00e744bd19a1b410e1384b4b47
715818885@qq.com <liangzhenghui@liangzhenghui-a.hnisi.com.cn>
2018-01-14 14:26:20 +0800

    区分ajax以及非ajax认证失败处理。

--- springsecurity-rest-validate-by-entrypoint/src/main/java/common/entrypoint/AppAuthenticationEntryPoint.java
+++ springsecurity-rest-validate-by-entrypoint/src/main/java/common/entrypoint/AppAuthenticationEntryPoint.java
@@ -7,11 +7,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.ELRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import common.util.ResponseUtil;
 
@@ -21,7 +24,8 @@ import common.util.ResponseUtil;
  */
 public class AppAuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
 
-    private final RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+	 private static final RequestMatcher requestMatcher = new ELRequestMatcher(
+	            "hasHeader('X-Requested-With','XMLHttpRequest')");
 
 
     /**
@@ -29,13 +33,22 @@ public class AppAuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
      */
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException) throws IOException, ServletException {
-
-    	//加上这一句就是开启http 基本认证,会弹出对验证对话框 
-    	//response.addHeader("WWW-Authenticate", "Basic realm=" + getRealmName());
-         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-         //PrintWriter writer = response.getWriter();
-         //writer.println("HTTP Status 401" + authException.getMessage());
-         ResponseUtil.renderJson(authException.getMessage(), response, null);
+    	  //判断是否为ajax请求，默认不是  
+    	/*boolean isAjaxRequest = false;  
+        if(!StringUtils.isBlank(request.getHeader("X-Requested-With")) && request.getHeader("X-Requested-With").equals("XMLHttpRequest")){  
+            isAjaxRequest = true;  
+        }*/
+        if (isRestRequest(request)) {
+        	//加上这一句就是开启http 基本认证,会弹出对验证对话框 
+        	//response.addHeader("WWW-Authenticate", "Basic realm=" + getRealmName());
+             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+             //PrintWriter writer = response.getWriter();
+             //writer.println("HTTP Status 401" + authException.getMessage());
+             ResponseUtil.renderJson(authException.getMessage(), response, null);
+        }else{
+        	response.sendRedirect(request.getContextPath()+"/403");
+        }
+    	
     }
     
     @Override
@@ -43,6 +56,13 @@ public class AppAuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
         setRealmName("Baeldung");
         super.afterPropertiesSet();
     }
-
+    /**
+     * Checks if it is a rest request
+     * @param request
+     * @return
+     */
+    protected boolean isRestRequest(HttpServletRequest request) {
+        return requestMatcher.matches(request);
+    }
 
 }
